2c1b727f7c525087342ef9af492368523a37fdfd
Snehal-Sabade <snehalsabade1947@gmail.com>
2023-05-04 23:22:37 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ApplyPlComponent } from './apply-pl/apply-pl.component';
+import { ExistingCustomerComponent } from './apply-pl/existing-customer/existing-customer.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'apply-pl',component:ApplyPlComponent},
+  { path:'existing-customer',component:ExistingCustomerComponent},
   {path:'',redirectTo:'/home',pathMatch:'full'}
 ];
 
--- src/app/apply-pl/existing-customer/existing-customer.component.ts
+++ src/app/apply-pl/existing-customer/existing-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-existing-customer',
@@ -8,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class ExistingCustomerComponent implements OnInit {
 
   constructor() { }
-
+  emailForm:any;
+  PhoneNumber!:number;
+  matcher:any;
   ngOnInit(): void {
+    let MOBILE_PATTERN = /[0-9\+\-\ ]/;
+
+    this.emailForm = new FormGroup({
+      PhoneNumber:new FormControl('',[Validators.pattern(MOBILE_PATTERN)])
+    });
+
   }
 
 }
